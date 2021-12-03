cd3e648d0b296c247b10e76e21ffca4c80b8dcaa
asosluev <a.sosluev@gmail.com>
2021-12-03 10:29:08 +0200

    111

--- setup.py
+++ setup.py
@@ -984,4 +984,4 @@ def web_hook():
 
 # application entry point
 if __name__ == '__main__':
-    server.run(host='0.0.0.0', port=int(os.environ.get('PORT', 8443)))
+    server.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
